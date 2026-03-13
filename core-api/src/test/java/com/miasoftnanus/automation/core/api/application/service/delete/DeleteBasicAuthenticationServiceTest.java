af4d8e1d59e6f3e8be2ad1b0afa1b4fa95f4f634
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-03-13 13:14:04 -0400

    refactor imports

--- core-api/src/test/java/com/miasoftnanus/automation/core/api/application/service/delete/DeleteBasicAuthenticationServiceTest.java
+++ core-api/src/test/java/com/miasoftnanus/automation/core/api/application/service/delete/DeleteBasicAuthenticationServiceTest.java
@@ -10,11 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteBasicAuthenticationServiceTest {
